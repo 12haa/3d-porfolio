3c508083dee173710973742752d2726b5e5a8564
12haa <12hadoto@gmail.com>
2023-11-21 10:21:39 +0330

    Add PWA?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "3D Porfolio",
+        short_name: "3D Porfolio",
+        description: "3D Porfolio",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/backend-565fc01f",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          // {
+          //   src: "/icons/backend-565fc01f",
+          //   sizes: "512x512",
+          //   type: "image/png",
+          // },
+          // {
+          //   src: "/icons/backend-565fc01f",
+          // },
+        ],
+      },
+    }),
+  ],
+});
